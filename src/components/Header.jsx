396e5773ba27c3900df85e36fcba3cfb14f8100a
thedarkvader321 <nicomunoz105@gmail.com>
2023-08-07 21:10:02 -0400

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Importamos el componente Link
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const Header = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const mostrarOcultarMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const select = () => {
+    setMenuVisible(false);
+  };
+
   return (
     <div className="contenedor-header">
       <header>
         <div className="logo">
-          <Link to="/">Confecciones Moni</Link> {/* Utilizamos Link en lugar de <a> */}
+          <Link to="/">Confecciones Moni</Link>
         </div>
-        <nav id="nav">
+        <div className="nav-responsive" onClick={mostrarOcultarMenu}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <nav id="nav" className={menuVisible ? "responsive" : ""}>
           <ul>
             <li>
-              <Link to="/">Inicio</Link> {/* Utilizamos Link en lugar de <a> */}
+              <Link to="/" onClick={select}>
+                Inicio
+              </Link>
             </li>
             <li>
-              <Link to="#sobremi">Poleras</Link> {/* Utilizamos Link en lugar de <a> */}
+              <Link to="#sobremi" onClick={select}>
+                Poleras
+              </Link>
             </li>
             <li>
-              <Link to="/agregar-poleras">Agregar Poleras</Link> {/* Utilizamos Link en lugar de <a> */}
+              <Link to="/agregar-poleras" onClick={select}>
+                Agregar Poleras
+              </Link>
             </li>
           </ul>
         </nav>
-        <div className="nav-responsive">
-          <i className="fa-solid fa-bars"></i>
-        </div>
       </header>
     </div>
   );
